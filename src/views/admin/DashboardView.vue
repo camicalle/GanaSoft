d230dac30aa7d96bf3a899456886d52b907f797c
Camiliño Calle <47997534+camicalle@users.noreply.github.com>
2023-05-03 21:34:13 -0500

    Clening

--- src/views/admin/DashboardView.vue
+++ src/views/admin/DashboardView.vue
@@ -4,7 +4,7 @@
     <div>
         <div class="flex flex-wrap">
             <div class="mt-4 w-full lg:w-6/12 xl:w-3/12 px-5 mb-4">
-                <div class="relative flex flex-col min-w-0 break-words bg-white rounded mb-3 xl:mb-0 shadow-lg">
+                <div class="relative flex flex-col min-w-0 break-words border-t-4 bg-white rounded mb-3 xl:mb-0 shadow-lg">
                     <div class="flex-auto p-4 pb-12">
                         <div class="flex flex-wrap">
                             <div class="relative w-full pr-4 max-w-full flex-grow flex-1">
@@ -12,14 +12,14 @@
                                 <span class="font-semibold text-xl text-blueGray-700">{{ quantityFarm }}</span>
                             </div>
                             <div class="relative w-auto pl-4 flex-initial">
-                                <div
+                                <a href="farms"
                                     class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full  bg-red-500">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                         stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                         <path stroke-linecap="round" stroke-linejoin="round"
                                             d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819" />
                                     </svg>
-                                </div>
+                                </a>
                             </div>
                         </div>
                     </div>
@@ -27,7 +27,7 @@
             </div>
 
             <div class="mt-4 w-full lg:w-6/12 xl:w-3/12 px-5 mb-4">
-                <div class="relative flex flex-col min-w-0 break-words bg-white rounded mb-3 xl:mb-0 shadow-lg">
+                <div class="relative flex flex-col min-w-0 break-words border-t-4 bg-white rounded mb-3 xl:mb-0 shadow-lg">
                     <div class="flex-auto p-4 pb-12">
                         <div class="flex flex-wrap">
                             <div class="relative w-full pr-4 max-w-full flex-grow flex-1">
@@ -35,14 +35,14 @@
                                 <span class="font-semibold text-xl text-blueGray-700">{{ quantityAnimal }}</span>
                             </div>
                             <div class="relative w-auto pl-4 flex-initial">
-                                <div
-                                    class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full  bg-red-500">
+                                <a href="/animals"
+                                    class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full  bg-pink-500">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                         stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                         <path stroke-linecap="round" stroke-linejoin="round"
                                             d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                                     </svg>
-                                </div>
+                                </a>
                             </div>
                         </div>
                     </div>
