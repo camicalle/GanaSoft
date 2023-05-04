67eca143492b7115ba273989fbf931e2c857fcb3
Camiliño Calle <47997534+camicalle@users.noreply.github.com>
2023-05-03 21:23:34 -0500

    Cleaning

--- src/views/admin/DashboardView.vue
+++ src/views/admin/DashboardView.vue
@@ -48,44 +48,6 @@
                     </div>
                 </div>
             </div>
-
-            <!-- <div class="mt-4 w-full lg:w-6/12 xl:w-3/12 px-5 mb-4">
-                <div class="relative flex flex-col min-w-0 break-words bg-white rounded mb-3 xl:mb-0 shadow-lg">
-                    <div class="flex-auto p-4 pb-12">
-                        <div class="flex flex-wrap">
-                            <div class="relative w-full pr-4 max-w-full flex-grow flex-1">
-                                <h5 class="text-blueGray-400 uppercase font-bold text-xs"> Fincas</h5>
-                                <span class="font-semibold text-xl text-blueGray-700">{{ quantityFarm }}</span>
-                            </div>
-                            <div class="relative w-auto pl-4 flex-initial">
-                                <div
-                                    class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full  bg-red-500">
-                                    <i class="fas fa-chart-bar"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="mt-4 w-full lg:w-6/12 xl:w-3/12 px-5 mb-4">
-                <div class="relative flex flex-col min-w-0 break-words bg-white rounded mb-3 xl:mb-0 shadow-lg">
-                    <div class="flex-auto p-4 pb-12">
-                        <div class="flex flex-wrap">
-                            <div class="relative w-full pr-4 max-w-full flex-grow flex-1">
-                                <h5 class="text-blueGray-400 uppercase font-bold text-xs"> Fincas</h5>
-                                <span class="font-semibold text-xl text-blueGray-700">{{ quantityFarm }}</span>
-                            </div>
-                            <div class="relative w-auto pl-4 flex-initial">
-                                <div
-                                    class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full  bg-red-500">
-                                    <i class="fas fa-chart-bar"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> -->
         </div>
     </div>
 </template>
