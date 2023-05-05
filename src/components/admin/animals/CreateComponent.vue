810f91170d8a9d21338e1e84162ef0f6c12a9403
Camiliño Calle <47997534+camicalle@users.noreply.github.com>
2023-05-05 00:55:53 -0500

    Changes

--- src/components/admin/animals/CreateComponent.vue
+++ src/components/admin/animals/CreateComponent.vue
@@ -16,7 +16,7 @@ onMounted(() => {
         <button
           data-modal-target="animalsCreateModal"
           data-modal-toggle="animalsCreateModal"
-          class="block font-bold text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          class="block font-bold text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
           type="button"
         >
           Agregar Animal
@@ -38,13 +38,13 @@ onMounted(() => {
             >
               <!-- Modal header -->
               <div
-                class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600"
+                class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600 bg-green-700"
               >
-                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">Agregar Animal</h3>
+                <h3 class="text-xl text-white font-semibold dark:text-white">Agregar Animal</h3>
                 <button
                   @click.prevent="close"
                   type="button"
-                  class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                  class="text-white bg-transparent hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
                   data-modal-hide="animalsCreateModal"
                 >
                   <svg
