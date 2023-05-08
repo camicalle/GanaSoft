d20340a95f1ebc7c29400e4989e731a434dcf3bb
josebarri <josealfredobarriossalgado@gmail.com>
2023-05-08 18:40:06 -0500

    test

--- src/components/admin/farms/CreateComponent.vue
+++ src/components/admin/farms/CreateComponent.vue
@@ -48,7 +48,7 @@ onMounted(() => {
                                     <div class="col-span-6 sm:col-span-6">
                                         <label for="dueño"
                                             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Seleccione
-                                            el Dueño</label>
+                                            el Dueños</label>
                                         <select v-model="farm.person" id="dueño" required
                                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                             <option value="" disabled selected>Seleccione el Dueño</option>
