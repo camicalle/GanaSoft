852e3f235d4b7fa265d656190911fbb4f782b5c5
Camiliño Calle <47997534+camicalle@users.noreply.github.com>
2023-05-09 14:06:47 -0500

    cleaning

--- src/components/admin/animals/ListComponent.vue
+++ src/components/admin/animals/ListComponent.vue
@@ -407,7 +407,7 @@
                     class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-bold rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Guardar
                   </button>
-                  <button @click="showPesoModal = false" type="button"
+                  <button @click="showPesoModal = false" v-on:click="close" type="button"
                     class="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-bold rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
                     Cancelar
                   </button>
@@ -509,6 +509,10 @@ export default {
             })
           }
         })
+    },
+    close() {
+        (this.peso.numero_kg = null),
+        (this.peso.fecha = '')
     },
     viewId(id) {
       const url = import.meta.env.VITE_BASE_URL;
